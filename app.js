780a6170bf355907a39480905e40e2ca1f7bfd18
Manas Paul <manas@codelogicx.com>
2021-07-24 22:31:56 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (_, res) => 
+    res.json({
+        message: 'We will be live soon!!!!!'
+    })
+)
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
